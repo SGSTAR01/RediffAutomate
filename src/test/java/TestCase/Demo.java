e6ec679a8a9348fd06113801987e99581e185090
Subham Ghosh <startroidx@gmail.com>
2026-01-14 07:17:40 +0000

    Added demo test case

--- src/test/java/TestCase/Demo.java
+++ src/test/java/TestCase/Demo.java
@@ -1,4 +1,13 @@
 package TestCase;
 
+import Browser.Browser;
+
+
 public class Demo {
+    public static void main(String[] args) {
+
+        Browser.openBrowser();
+        Browser.title("Rediff.com: News | Rediffmail | Stock Quotes | Rediff Gurus");
+        Browser.closeBrowser();
+    }
 }
